b01ca4f165807ea628b866d8b871ca11487b9c2f
smoreno10 <morenosanti@hotmail.com>
2022-04-17 19:32:20 -0300

    color CartWidget

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CartWidget = ({conteo}) => {
   return (
-    <button type="button" class="btn btn-dark position-relative">
+    <button type="button" class="btn btn-warning position-relative">
     <i className="fa-solid fa-cart-shopping" />
     <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
       {conteo}
